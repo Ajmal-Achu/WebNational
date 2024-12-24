dbd9c25a4c9089abe98f796fbf532eae0e8bbee3
Ajmal <88565942+Ajmal-Achu@users.noreply.github.com>
2024-12-24 22:42:21 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import Spline from '@splinetool/react-spline';
 const Hero = () => {
   return (
     <Spline
-        scene="https://prod.spline.design/dbJJOL6wsAQd4OYx/scene.splinecode" 
+        scene="" 
       />
   );
 };
