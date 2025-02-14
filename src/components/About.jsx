2dd7ffd156019072d23d449d3a737d2865b089e7
Ajmal <88565942+Ajmal-Achu@users.noreply.github.com>
2025-02-14 12:53:07 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,119 +1,94 @@
-import { motion } from "framer-motion"
-import ServiceCard from "./ServiceCard"
-import { backend, web, mobile, creator } from "../assets"
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { fadeIn, textVariant } from "../utils/motion";
+import { personalInfo, navigationPaths, services } from "../constants";
+import { Tilt } from "react-tilt";
+import { SectionWrapper } from "../hoc";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt
+      className="xs:w-[250px] w-full"
+      options={{ max: 45, scale: 1, speed: 450 }}
+    >
+      <motion.div
+        variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div className="bg-tertiary rounded-[20px] min-h-[280px] py-5 px-12 flex flex-col justify-evenly items-center">
+          <img
+            src={icon}
+            alt="web-development"
+            className="w-16 h-16 object-contain"
+          />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
+
 const About = () => {
   const handleLogin = () => {
-    window.open("https://cmt3.research.microsoft.com/NCICPS2025", "_blank", "noopener,noreferrer")
-  }
-
-  const textVariant = () => {
-    return {
-      hidden: {
-        y: 50,
-        opacity: 0,
-      },
-      visible: {
-        y: 0,
-        opacity: 1,
-        transition: {
-          duration: 0.8,
-          ease: "easeInOut",
-        },
-      },
-    }
-  }
-
-  const fadeIn = (direction, type, delay, duration) => {
-    return {
-      hidden: {
-        x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
-        y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
-        opacity: 0,
-      },
-      show: {
-        x: 0,
-        y: 0,
-        opacity: 1,
-        transition: {
-          type: type,
-          delay: delay,
-          duration: duration,
-          ease: "easeOut",
-        },
-      },
-    }
-  }
-
-  const services = [
-    {
-      title: "Cutting-edge research presentations",
-      icon: backend,
-    },
-    {
-      title: "Networking opportunities",
-      icon: mobile,
-    },
-    {
-      title: "Advance interactive workshops",
-      icon: web,
-    },
-    {
-      title: "Industry-academia collaborations",
-      icon: creator,
-    },
-  ]
+    window.open("https://cmt3.research.microsoft.com/NCICPS2025", "_blank", "noopener,noreferrer");
+  };
 
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full">
-      <motion.div variants={textVariant()} className="mt-4 w-full text-center">
-        <h2 className="text-4xl font-bold text-white mb-4 text-center">About the Conference</h2>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className="text-4xl font-bold text-white mb-8">
+          About Us
+        </h2>
       </motion.div>
+
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className="mt-2 text-secondary text-[17px] w-full max-w-4xl leading-[30px] text-left"
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        Cyber-physical security is crucial in our interconnected world, where digital systems integrate with physical
-        infrastructure. As technology becomes more embedded in daily life, vulnerabilities emerge. A comprehensive
-        strategy combining cyber and physical security is essential. The 2nd National Conference on Intelligent
-        Cyber-Physical Systems (NCICP525) gathers researchers, practitioners, and innovators in cyber-physical systems
-        and intelligent applications. Join us in shaping the future of these systems and their impact on our
-        interconnected world.
+        {personalInfo.projectsIntro}
       </motion.p>
 
-      <motion.div variants={textVariant()} className="mt-4 w-full text-center">
-        <h2 className="text-4xl font-bold text-white mb-4 text-center">Call For Papers</h2>
+      <motion.div variants={textVariant()}>
+        <h2 className="text-4xl font-bold text-white mb-8">
+          Call For Papers
+        </h2>
       </motion.div>
+
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className="mt-2 text-secondary text-[17px] w-full max-w-4xl leading-[30px] text-left"
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        NCICPS'25 invites full paper submissions in various tracks. All accepted papers will be published in the
-        conference proceedings, and selected papers will be published in the International Journal of Science Technology
-        and Innovation (IJSTI). We welcome innovative research, case studies, and practical applications in the fields
-        of Intelligent Cyber Physical Systems, Computational Intelligence, and Cyber Security & Forensics.
+        {personalInfo.about}
       </motion.p>
 
-      <motion.div variants={textVariant()} className="mt-6 w-full text-center">
-        <h2 className="text-4xl font-bold text-white mb-4 text-center">Important Dates</h2>
+      <motion.div variants={textVariant()} className="mt-10">
+        <h2 className="text-4xl font-bold text-white mb-8">
+          Important Dates
+        </h2>
       </motion.div>
+
       <motion.ul
         variants={fadeIn("", "", 0.1, 1)}
-        className="mt-2 text-secondary text-[17px] w-full max-w-4xl leading-[30px] list-disc list-inside text-left"
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px] list-disc list-inside"
       >
         <li>Paper Submission Deadline: 21/01/2025</li>
-        <li>Notification of acceptance: 14/02/2025</li>
-        <li>Camera-Ready paper submission: 18/02/2025</li>
-        <li>Registration for conference: 20/02/2025</li>
+        <li>Notification of acceptance: 10/02/2025</li>
+        <li>Camera-Ready paper submission: 15/02/2025</li>
+        <li>Registration for conference: 15/02/2025</li>
         <li>Registration closes on: 20/02/2025</li>
       </motion.ul>
 
       <motion.div
         variants={fadeIn("", "", 0.1, 1)}
-        className="mt-4 text-secondary text-[17px] w-full max-w-4xl leading-[30px] text-left"
+        className="mt-6 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
         <p>Authors can submit papers through Microsoft CMT.</p>
         <motion.button
-          className="mt-3 bg-gradient-to-r from-purple-500 to-indigo-600 text-white py-2 px-6 rounded-full font-semibold shadow-lg hover:shadow-xl transition-all duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105"
+          className="mt-4 bg-gradient-to-r from-purple-500 to-indigo-600 text-white py-2 px-6 rounded-full font-semibold shadow-lg hover:shadow-xl transition-all duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105"
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.95 }}
           onClick={handleLogin}
@@ -122,14 +97,14 @@ const About = () => {
         </motion.button>
       </motion.div>
 
-      <div className="mt-12 flex flex-wrap justify-center gap-10">
+      <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
       </div>
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default About
+export default SectionWrapper(About, navigationPaths.about);
 
